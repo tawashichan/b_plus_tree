0044335ed50af0bcf06e98990ea6b0b6e0e7cf94
irieryota <ryotairie123@gmail.com>
2021-09-25 19:38:54 +0900

    feat: handle random insert

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, vec};
 use thiserror::Error;
 
-type Key = String;
+type Key = usize;
 
 type NodeId = usize;
 
@@ -57,7 +57,7 @@ impl BTree {
         }
     }
 
-    fn insert(&mut self, key: &String, value: &String) -> Result<(), Error> {
+    fn insert(&mut self, key: &Key, value: &String) -> Result<(), Error> {
         let insert_result = self.insert_rec(self.root_id, key, value)?;
         match insert_result {
             Some((up_key, right_node_id)) => {
@@ -75,7 +75,7 @@ impl BTree {
     fn insert_rec(
         &mut self,
         current_node_id: NodeId,
-        key: &String,
+        key: &Key,
         value: &String,
     ) -> Result<Option<(Key, NodeId)>, Error> {
         println!("node_id: {:?} self:{:?}", current_node_id, self);
@@ -83,18 +83,18 @@ impl BTree {
         let current_node = self.node_map.get_mut(&current_node_id).unwrap();
         let insert_result = match current_node {
             Node::Leaf(id, keys, sibling) => {
-                // todo 右端以外にinsertのパターンに対応
-                keys.push(key.clone());
+                insert_key(key.clone(), keys);
 
                 if keys.len() < self.node_key_num {
                     None
                 } else {
                     let new_node_keys = keys.split_off(self.node_key_num / 2);
                     let new_node_id = self.node_id_generator.generate_node_id();
+                    let existing_sibling = sibling.clone();
                     *sibling = Some(new_node_id);
 
                     let min_key = new_node_keys.first().unwrap().clone();
-                    let new_node = Node::Leaf(new_node_id, new_node_keys, None);
+                    let new_node = Node::Leaf(new_node_id, new_node_keys, existing_sibling);
 
                     self.node_map.insert(new_node_id, new_node);
 
@@ -129,14 +129,13 @@ impl BTree {
                     Node::Leaf(_, _, _) => Ok(Some((min_key, right_node_id))),
                     Node::Mid(_, keys, node_ids) => {
                         println!("min_key: {:?} right_node_id: {:?}", min_key, right_node_id);
-                        // todo  右端への追加でないパターンに対応
-                        keys.push(min_key);
-                        node_ids.push(right_node_id);
+                        insert_key_node_id(min_key, right_node_id, keys, node_ids);
 
                         if keys.len() < self.node_key_num {
                             Ok(None)
                         } else {
                             // 葉ノードを二つに分割し,右側の最小値を親ノードに持っていく
+                            // めちゃくちゃ適当に書いているので治安が終了している
                             let mut new_node_keys_tmp = keys.split_off(self.node_key_num / 2);
                             let new_node_keys = new_node_keys_tmp.split_off(1);
                             let up_key = new_node_keys_tmp[0].clone();
@@ -160,9 +159,51 @@ impl BTree {
     }
 }
 
+fn insert_key(key: Key, keys: &mut Vec<Key>) {
+    let len = keys.len();
+    if len == 0 {
+        keys.push(key);
+        return;
+    }
+
+    let mut location = keys.len();
+    for (i, k) in keys.iter().enumerate() {
+        if &key <= k {
+            location = i;
+        }
+    }
+    let mut separated_keys = keys.split_off(location);
+
+    keys.push(key);
+    keys.append(&mut separated_keys);
+}
+
+fn insert_key_node_id(key: Key, node_id: NodeId, keys: &mut Vec<Key>, node_ids: &mut Vec<NodeId>) {
+    let len = keys.len();
+    if len == 0 {
+        keys.push(key);
+        return;
+    }
+
+    let mut location = keys.len();
+    for (i, k) in keys.iter().enumerate() {
+        if &key <= k {
+            location = i;
+        }
+    }
+    let mut separated_keys = keys.split_off(location);
+
+    keys.push(key);
+    keys.append(&mut separated_keys);
+
+    let mut separated_node_ids = node_ids.split_off(location + 1);
+    node_ids.push(node_id);
+    node_ids.append(&mut separated_node_ids);
+}
+
 fn main() {
     let mut t = BTree::new(3);
-    t.insert(&"1".to_string(), &"1".to_string());
+    /*  t.insert(&"1".to_string(), &"1".to_string());
     t.insert(&"3".to_string(), &"3".to_string());
 
     println!("\n insert 5");
@@ -172,6 +213,12 @@ fn main() {
     t.insert(&"7".to_string(), &"7".to_string());
 
     println!("\n insert 9");
-    t.insert(&"9".to_string(), &"9".to_string());
+    t.insert(&"9".to_string(), &"9".to_string());*/
+    t.insert(&20, &"20".to_string());
+    t.insert(&7, &"7".to_string());
+    t.insert(&13, &"13".to_string());
+    t.insert(&2, &"2".to_string());
+    t.insert(&10, &"10".to_string());
+    t.insert(&8, &"8".to_string());
     println!("{:?}", t);
 }
